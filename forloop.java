2679bcd30a8f90f6f808d3b7941150cd1f5f6552
sirijan <132748518+sirijan@users.noreply.github.com>
2024-05-30 15:41:26 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,12 @@
+public class forloop {
+    public static void main(String[] args){
+//        for(int i=10;i<=50;i++)
+//            System.out.print(" "+i);
+        int i=10;
+        while(i<=50){
+
+            System.out.print(" "+i);
+            i++;
+        }
+    }
+}
