69b97331ee9be0803ae0b3b9fbf6c13d9bd8408a
sirijan <132748518+sirijan@users.noreply.github.com>
2024-06-04 09:30:31 +0530

    Add files via upload
    Oops

---
+++ Person.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Person {
+    //instance variable
+    Scanner sc = new Scanner(System.in);
+    public String name = sc.nextLine();
+    public int age = 18;
+    //Default Constructor
+    public Person(){
+
+    }
+    //Method to display output
+    public void display(){
+//    System.out.print(name);
+    System.out.println("Age: "+age);
+    }
+    public static void main(String[] args){
+        System.out.print("Enter the name: ");
+        Person person = new Person();
+        person.display();
+    }
+}
